e714dd61f008c02fb51b41d9bc3b2170915abf05
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2024-05-31 13:22:02 +0100

    restructure and add my-banks page

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,16 +1,19 @@
 import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
   userName,
   showBalance = true,
 }: CreditCardProps) => {
-  // console.log(account);
   return (
     <div className="flex flex-col">
-      <Link href={`/}`} className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
@@ -52,6 +55,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.shareableId} />}
     </div>
   );
 };
